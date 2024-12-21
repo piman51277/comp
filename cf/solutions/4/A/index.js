cda6f3d5d5fe13b7084d664d7eac24ce1acecf1b
piman51277 <piman@piman.dev>
2024-12-21 10:56:06 -0500

    4A

---
+++ cf/solutions/4/A/index.js
@@ -0,0 +1,34 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let inputString = "";
+let currentLine = 0;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+  main();
+});
+function readline() {
+  return inputString[currentLine++];
+}
+const readInt = () => parseInt(readline());
+const readIntArr = () => readline().split(" ").map(Number);
+const cl = console.log;
+
+function main() {
+  //code here
+  //debug using node <filename>.js < input.txt
+  const n = readInt();
+  if (n % 2 == 0 && n > 2) {
+    cl("YES");
+  } else {
+    cl("NO");
+  }
+}
