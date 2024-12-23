e0748f7d7c7024b402c75b470d3a628894b02122
piman51277 <piman@piman.dev>
2024-12-23 00:38:07 -0500

    rm silent fail

--- cf/tools/scripts/run_cpp.sh
+++ cf/tools/scripts/run_cpp.sh
@@ -1,3 +1,3 @@
 CXXFLAGS="-O3 -march=native -mavx2 -Wall --std=c++17 -fsanitize=address,undefined,leak"
-rm *.out
+rm *.out 2> /dev/null
 g++ index.cpp -o index.out $CXXFLAGS && ./index.out < input.txt
